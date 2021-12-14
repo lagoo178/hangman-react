f4a1e1780cb3f8f1c3ea502901f7fe3acf4fb267
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-12-14 13:47:27 +0700

    finished the project

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// rafce
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <h3>By lagoo178</h3>
+      <p>Find the hidden word - Enter a letter</p>
+    </>
+  )
+}
+
+export default Header
